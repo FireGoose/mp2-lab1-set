9ea745f49387ba504cec39e3fe6b139d5a884910
Andrey Gerasimov <81647354+FireGoose@users.noreply.github.com>
2025-09-27 22:39:30 +0300

    Update tbitfield.h

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -1,12 +1,4 @@
-// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// tbitfield.h - Copyright (c) Гергель В.П. 07.05.2001
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
-// Битовое поле
-
-#ifndef __BITFIELD_H__
-#define __BITFIELD_H__
+#pragma once
 
 #include <iostream>
 
@@ -26,7 +18,7 @@ private:
   TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
 public:
   TBitField(int len);                //                                   (#О1)
-  TBitField(const TBitField &bf);    //                                   (#П1)
+  TBitField(const TBitField& bf);    //                                   (#П1)
   ~TBitField();                      //                                    (#С)
 
   // доступ к битам
@@ -36,15 +28,15 @@ public:
   int  GetBit(const int n) const; // получить значение бита               (#Л1)
 
   // битовые операции
-  int operator==(const TBitField &bf) const; // сравнение                 (#О5)
-  int operator!=(const TBitField &bf) const; // сравнение
-  TBitField& operator=(const TBitField &bf); // присваивание              (#П3)
-  TBitField  operator|(const TBitField &bf); // операция "или"            (#О6)
-  TBitField  operator&(const TBitField &bf); // операция "и"              (#Л2)
+  int operator==(const TBitField& bf) const; // сравнение                 (#О5)
+  int operator!=(const TBitField& bf) const; // сравнение
+  TBitField& operator=(const TBitField& bf); // присваивание              (#П3)
+  TBitField  operator|(const TBitField& bf); // операция "или"            (#О6)
+  TBitField  operator&(const TBitField& bf); // операция "и"              (#Л2)
   TBitField  operator~(void);                // отрицание                  (#С)
 
-  friend istream &operator>>(istream &istr, TBitField &bf);       //      (#О7)
-  friend ostream &operator<<(ostream &ostr, const TBitField &bf); //      (#П4)
+  friend istream& operator>>(istream& istr, TBitField &bf);       //      (#О7)
+  friend ostream& operator<<(ostream& ostr, const TBitField &bf); //      (#П4)
 };
 // Структура хранения битового поля
 //   бит.поле - набор битов с номерами от 0 до BitLen
